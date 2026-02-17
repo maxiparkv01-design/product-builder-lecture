0ef6c7ae56e90a71c1f464ffce98a1f648a4a40a
cubicpink <cubicpink@gmail.com>
2026-02-17 11:05:02 +0000

    Add dark and light mode toggle functionality

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const generateBtn = document.getElementById('generate-btn');
   const numbersContainer = document.getElementById('numbers');
+  const themeSwitchBtn = document.getElementById('theme-switch-btn');
+
+  // Theme switch functionality
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+
+  themeSwitchBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    let theme = 'light';
+    if (document.body.classList.contains('dark-mode')) {
+      theme = 'dark';
+    }
+    localStorage.setItem('theme', theme);
+  });
+
 
   generateBtn.addEventListener('click', () => {
     generateLottoNumbers();
